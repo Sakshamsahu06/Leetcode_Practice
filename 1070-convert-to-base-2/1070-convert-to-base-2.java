1d7df2581759dfcfd4cbc98c2ba7c0d845214de7
Saksham <76005828+Sakshamsahu06@users.noreply.github.com>
2023-11-02 21:13:41 +0530

    Time: 0 ms (100%), Space: 39.9 MB (44.96%) - LeetHub

---
+++ 1070-convert-to-base-2/1070-convert-to-base-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String baseNeg2(int n) {
+        StringBuilder sc = new StringBuilder();
+        int rem;
+        int b = -2;
+        if(n == 0) sc.append('0');
+        
+        while(n != 0){
+            rem = n % b;
+            n /= b;
+            if(rem < 0){
+                rem -= b;
+                n++;
+            }
+            sc.append(rem);
+        }
+        return sc.reverse().toString();
+    }
+}
